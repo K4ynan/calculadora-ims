28fd89b6319fec6ebd7d83c675aee0efc3acac6d
K4ynan <kaynancamilo12@gmail.com>
2022-09-19 15:05:02 -0300

    calculadora-imc

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function calcular () {
+function calcular() {
     let saida = document.getElementById('saida')
     let peso = Number(document.getElementById('peso').value)
     let altura = Number(document.getElementById('altura').value)
@@ -7,13 +7,22 @@ function calcular () {
 
     if (soma <= 18.5) {
         saida.innerHTML += `<p>Seu teste deu ${soma}. Abaixo do peso`
-}
-else if (soma >= 18.6 && soma <= 24.9) {
-    saida.innerHTML += `<p>Seu teste deu ${soma}. Peso ideal (Parabéns)`
-}
-else if (soma >= 25 && soma <= 29.9) {
-    saida.innerHTML += `<p>`
-}
+    }
+    else if (soma >= 18.6 && soma <= 24.9) {
+        saida.innerHTML += `<p>Seu teste deu ${soma}. Peso ideal (Parabéns)`
+    }
+    else if (soma >= 25 && soma <= 29.9) {
+        saida.innerHTML += `<p> Seu teste deu ${soma}. Levemente acima do peso`
+    }
+    else if (soma >= 30 && soma <= 34.9) {
+        saida.innerHTML += `<p> Seu teste deu ${soma}. Obesidade grau I`
+    }
+    else if (soma >= 35 && soma <= 39, 9) {
+        saida.innerHTML += `<p>Seu teste deu ${soma}. Obesidade grau II`
+    }
+    else {
+        saida.innerHTML += `<p>Seu teste deu ${soma}. Obesidade grau III`
+    }
 }
 
 
